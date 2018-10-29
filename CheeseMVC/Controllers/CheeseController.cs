8841d084d37fa39c42c22e2adba03533b442e2fc
JaneWB <brownjannick@att.net>
2018-10-29 13:20:32 -0500

    Cheese MVC CG 16-2

---
+++ CheeseMVC/Controllers/CheeseController.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace CheeseMVC.Controllers
+{
+    public class CheeseController : Controller
+    {
+        static public Dictionary<string, string> Cheeses = new Dictionary<string, string>();
+
+        // GET: /<controller>/
+        public IActionResult Index()
+        {
+            ViewBag.cheeses = Cheeses;
+
+            return View();
+        }
+
+        public IActionResult Add()
+        {
+            return View();
+        }
+
+        public IActionResult Remove()
+        {
+            ViewBag.cheeses = Cheeses;
+
+            return View();
+        }
+
+        [HttpPost]
+        [Route("/Cheese/Add")]
+        public IActionResult NewCheese(string name, string description)
+        {
+            //Add new cheese to existing cheeses
+            Cheeses.Add(name, description);
+
+            return Redirect("/Cheese");
+        }
+
+       
+
+        [HttpPost]
+        [Route("/Cheese/Remove")]
+        public IActionResult RemoveCheese(string name)
+        {
+            //Remove cheese from list of cheeses
+
+            Cheeses.Remove(name);
+
+            return Redirect("/Cheese");
+            
+        }
+       
+    }
+}
